ec674e38fbb2199e0003cd14192840cb9b717294
gojoego <josephjgallego@gmail.com>
2025-11-25 23:55:41 -0600

    IsUnique problem set solution 1

--- backend/src/main/java/com/ctci/ch1/IsUnique.java
+++ backend/src/main/java/com/ctci/ch1/IsUnique.java
@@ -1,5 +1,20 @@
 package com.ctci.ch1;
 
 public class IsUnique {
-    
+    public static void main(String[] args) {
+        System.out.println(isUnique(""));
+    }
+
+    public static boolean isUnique(String input) {
+        if (input.length() > 128) return false;
+
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < input.length(); i++) {
+            int value = input.charAt(i);
+            if (char_set[value]) return false;
+
+            char_set[value] = false;
+        }
+        return true;
+    }
 }
