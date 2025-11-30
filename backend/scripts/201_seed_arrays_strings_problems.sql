016ea84b43733a5c2992176d70fd6c35cf845718
gojoego <josephjgallego@gmail.com>
2025-11-30 13:39:10 -0600

    problem sets

--- backend/scripts/201_seed_arrays_strings_problems.sql
+++ backend/scripts/201_seed_arrays_strings_problems.sql
@@ -1,7 +1,17 @@
-\set unique_prompt  'Implement an algorithm to determine if a string has all unique characters. 
+\set is_unique      'Implement an algorithm to determine if a string has all unique characters. 
                     What if you cannot use additional data structures?'
 
-\set check_prompt   'Given 2 strings, write a method to decide if one is a permutation of the other.'
+\set check_perm     'Given 2 strings, write a method to decide if one is a permutation of the other.'
+
+\set urlify         'Write a method to replace all spaces in a string with '%20' - you may assume that the string 
+                    has sufficient space at the end to hold the additional characters, and that you are given the "true"
+                    length of the string. (Note: If implementing in Java, please use a character array so that you can 
+                    perform this oepration in place.)'
+
+\set palin_perm     'Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a 
+                    word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
+                    The palindrome does not need to be limited to just dictionary words. You can ignore casing and 
+                    non-letter characters.'
 
 \set str_compress   'Implement a method to perform basic string compression using the counts of repeated characters.
                     For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not 
--- backend/src/main/java/com/ctci/ch1/IsUnique.java
+++ backend/src/main/java/com/ctci/ch1/IsUnique.java
@@ -2,10 +2,11 @@ package com.ctci.ch1;
 
 public class IsUnique {
     public static void main(String[] args) {
-        System.out.println(isUnique(""));
+        System.out.println(isUniqueChars(""));
+        System.out.println(isUniqueCharsBit(""));
     }
 
-    public static boolean isUnique(String input) {
+    public static boolean isUniqueChars(String input) {
         if (input.length() > 128) return false;
 
         boolean[] char_set = new boolean[128];
@@ -13,8 +14,20 @@ public class IsUnique {
             int value = input.charAt(i);
             if (char_set[value]) return false;
 
-            char_set[value] = false;
+            char_set[value] = true;
         }
+        return true;
+    }
+
+    public static boolean isUniqueCharsBit(String input) {
+        int checker = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            int value = input.charAt(i);
+            if ((checker & (1 << value)) > 0) return false;
+            checker |= (1 << value); 
+        }
+    
         return true;
     }
 }
